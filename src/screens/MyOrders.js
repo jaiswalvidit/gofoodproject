cfbe9cc38155d0ec7cfba25b4947a337142cfe57
jaiswalvidit <jaiswalvidit23@gmail.com>
2023-12-02 14:55:04 +0530

    build added

--- src/screens/MyOrders.js
+++ src/screens/MyOrders.js
@@ -4,46 +4,36 @@ import { IMAGE_URL } from '../constants';
 
 export default function MyOrder() {
   const [orderData, setOrderData] = useState({ email: '', products: [] });
-
-  const fetchMyOrder = async () => {
-    try {
-      const userEmail = localStorage.getItem('userEmail');
-
-<<<<<<< HEAD
-      // Ensure userEmail is present before making the request
-      if (!userEmail) {
-        console.error('User email not found in localStorage');
-        return;
-      }
-
-=======
->>>>>>> 4c34730875cbe214882fb29291323b21218e7d14
-      const response = await fetch(`http://localhost:5000/api/myOrderData?email=${userEmail}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-<<<<<<< HEAD
-=======
-      
-      
-
->>>>>>> 4c34730875cbe214882fb29291323b21218e7d14
-      if (response.ok) {
-        const data = await response.json();
-        setOrderData(data.orderData[0] || { email: '', products: [] });
-      } else {
-        console.error('Fetch failed with status:', response.status);
-        setOrderData({ email: 'Email not found', products: [] });
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      setOrderData({ email: 'Email not found', products: [] });
-    }
-  };
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    const fetchMyOrder = async () => {
+      try {
+        const userEmail = localStorage.getItem('userEmail');
+
+        const response = await fetch(`http://localhost:5000/api/myOrderData?email=${userEmail}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setOrderData(data.orderData[0] || { email: '', products: [] });
+        } else {
+          console.error('Fetch failed with status:', response.status);
+          setError('Failed to fetch data');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        setError('An unexpected error occurred');
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchMyOrder();
   }, []);
 
@@ -73,54 +63,64 @@ export default function MyOrder() {
       <div className="container my-2">
         <div className="row">
           <div className="col-12">
-            <ul className="list-unstyled">
-              {orderData.products && orderData.products.length === 0 ? (
-                <li>No product added</li>
-              ) : (
-                orderData.products.map((productGroup, index) => (
-                  <li key={index} className="mb-4">
-                    <div className="m-2 p-2" style={cardStyle}>
-                      {productGroup[0].Order_date && (
-                        <div style={dateStyle} className="fs-4">
-                          Date: {new Date(productGroup[0].Order_date).toLocaleDateString()} Time:{' '}
-                          {new Date(productGroup[0].Order_date).toLocaleTimeString()}
-                        </div>
-                      )}
-                      <ul className="list-unstyled m-3">
-                        {productGroup.slice(1).map((product, subIndex) => (
-                          <li key={subIndex} className="mb-3">
-                            <div className="rpw" style={productCardStyle}>
-                              <div className="text-start fs-6 col-9">
-                                <p className="text-success font-weight-bold mb-0">
-                                  <strong>Name: {product.Name}</strong>
-                                </p>
-                                <p className="text-danger mb-0">
-                                  <strong>Price: ₹{product.price / 100}/-</strong>
-                                </p>
-                                <p className="text-dark mb-0">
-                                  <strong>Quantity: {product.qty}</strong>
-                                </p>
-                                <p className="text-dark mb-0">
-                                  <strong>Description: {product.description}</strong>
-                                </p>
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <>
+                {error ? (
+                  <div>Error: {error}</div>
+                ) : (
+                  <ul className="list-unstyled">
+                    {orderData.products && orderData.products.length === 0 ? (
+                      <li>No product added</li>
+                    ) : (
+                      orderData.products.map((productGroup, index) => (
+                        <li key={index} className="mb-4">
+                          <div className="m-2 p-2" style={cardStyle}>
+                            {productGroup[0].Order_date && (
+                              <div style={dateStyle} className="fs-4">
+                                Date: {new Date(productGroup[0].Order_date).toLocaleDateString()} Time:{' '}
+                                {new Date(productGroup[0].Order_date).toLocaleTimeString()}
                               </div>
-                              <div className="text-end col-3 rounded ">
-                                <img 
-                                  src={`${IMAGE_URL}${product.imageId || 'defaultImageId'}`}
-                                  alt={`Product ${product.Name}`}
-                                  style={{ maxWidth: '100%', height: 'auto' }}
-                                  className="img-fluid  rounded"
-                                />
-                              </div>
-                            </div>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  </li>
-                ))
-              )}
-            </ul>
+                            )}
+                            <ul className="list-unstyled m-3">
+                              {productGroup.slice(1).map((product, subIndex) => (
+                                <li key={subIndex} className="mb-3">
+                                  <div className="rpw" style={productCardStyle}>
+                                    <div className="text-start fs-6 col-9">
+                                      <p className="text-success font-weight-bold mb-0">
+                                        <strong>Name: {product.Name}</strong>
+                                      </p>
+                                      <p className="text-danger mb-0">
+                                        <strong>Price: ₹{product.price / 100}/-</strong>
+                                      </p>
+                                      <p className="text-dark mb-0">
+                                        <strong>Quantity: {product.qty}</strong>
+                                      </p>
+                                      <p className="text-dark mb-0">
+                                        <strong>Description: {product.description}</strong>
+                                      </p>
+                                    </div>
+                                    <div className="text-end col-3 rounded ">
+                                      <img
+                                        src={`${IMAGE_URL}${product.imageId || 'defaultImageId'}`}
+                                        alt={`Product ${product.Name}`}
+                                        style={{ maxWidth: '100%', height: 'auto' }}
+                                        className="img-fluid  rounded"
+                                      />
+                                    </div>
+                                  </div>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        </li>
+                      ))
+                    )}
+                  </ul>
+                )}
+              </>
+            )}
           </div>
         </div>
       </div>
